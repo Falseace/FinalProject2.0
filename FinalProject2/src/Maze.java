39bb563cebba80b64104826729a299d3b3b3ad97
Falseace <falseace@gmail.com>
2018-05-16 14:02:55 -0600

    Maze Array List

--- FinalProject2/src/Maze.java
+++ FinalProject2/src/Maze.java
@@ -1,6 +1,63 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
+
 public class Maze {
+    private Tile[][] multi;
+    private int w;
+    private int h;
     public Maze()
     {
+        h=7;
+        w=7;
+        multi=new Tile[h][w];
+        makeMaze();
+    }
+    public void makeMaze()
+    {
+        for(int i=0;i<h;i++)
+        {
+            for(int j=0;j<w;j++)
+            {
+                if(i==0) {
+
+                }
+                else if(j==0)
+                {
+
+                }
+                else if(i==h-1)
+                {
+
+                }
+                else(j==w-1)
+                {
+
+                }
+            }
+        }
+    }
+    public JFrame addMaze(JFrame J)
+    {
+        for(int i=0;i<h;i++)
+        {
+            for(int j=0;j<w;j++)
+            {
+                J.add(multi[i][j]);
+            }
+        }
+        return J;
+    }
+    public int getHeight()
+    {
+        return multi.length;
+    }
+    public static void main(String[]args)
+    {
+        Maze m=new Maze();
+        JFrame J=new JFrame("Hello");
+        //J.set
+        J.setVisible(true);
+
 
     }
 }
